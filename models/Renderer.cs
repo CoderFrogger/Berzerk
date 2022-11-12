8080504842268bc730fc82450357af9c3c182ad4
CoderFrogg <ignas.paulauskis@stud.vilniustech.lt>
2022-11-12 19:35:06 +0200

    Rendered

---
+++ models/Renderer.cs
@@ -0,0 +1,40 @@
+public class Renderer{
+    private List<Enemy> enemies;
+    private Player player;
+    private Maze maze;
+
+    public Renderer(Player player, Maze maze, List<Enemy> enemies){
+    this.player = player;
+    this.maze = maze;
+    this.enemies = enemies;
+    }
+
+    public void renderGame() {
+    for (int y=0; y<maze.getHeight(); y++) {
+            for (int x=0; x<maze.getWidth(); x++) {
+
+                if (y == player.y && x == player.x)
+                    Console.Write("C");
+
+                else if (isEnemy(x, y))
+                    Console.Write("@");
+
+                else if (maze.isWall(x, y))
+                    Console.Write("#");
+
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private bool isEnemy(int x, int y) {
+        foreach (Enemy enemy in enemies) {
+            if (enemy.x == x && enemy.y == y)
+            return true;
+        }
+        return false;
+    }
+    
+}
